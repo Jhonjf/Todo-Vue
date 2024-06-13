9a4ca3c7c750a49033922f6331e7c26e69707e4b
Jhon <jhonatanjf0408@gmail.com>
2024-06-13 09:14:43 -0300

    atualizando

--- src/components/Cabecalho.vue
+++ src/components/Cabecalho.vue
@@ -1,12 +1,14 @@
 <script setup>
-    const props = defineProps(['tarefasPendentes'])
+    import { defineProps } from 'vue';
+
+    const props = defineProps(['tarefasPendentes']);
 </script>
 
 <template>
-    <header class="p-5 mb-4 mt-4 bg-light roudend-3" >
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
         <h1>Minhas tarefas</h1>
         <p>
-            Você possui {{ props.tarefasPendentes().length }} tarefas pendentes
+            Você possui {{ props.tarefasPendentes.length }} tarefas pendentes
         </p>
     </header>
 </template>
